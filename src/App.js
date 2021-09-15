8721415b312e5d7e83c08cb94f41646f8faccdbb
cosmina iosifescu <cosmina.iosifescu@gmail.com>
2021-09-15 14:23:51 +0200

    sorted out scss files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import './stylesheets/App.scss';
+import '../src/stylesheets/App.scss';
 import Footer from  './components/Footer';
 import AboutPage from  './pages/AboutPage';
 import LandingPage from './pages/LandingPage';
@@ -11,11 +11,11 @@ import Sidebar from './components/Sidebar';
 function App() {
   return (
       <div className="App">
-      <a href="/"><i className="logo logo-bright fas fa-fan"></i></a>
+      <a href="/portfolio"><i className="logo logo-bright fas fa-fan"></i></a>
           <Router>
             <Switch>
               <Route path="/about" component={AboutPage} />
-              <Route path="/" exact component={LandingPage} />
+              <Route path="/portfolio" component={LandingPage} />
               <Route path="/myprojects" component={MyProjectsPage} />
               <Route path="/skills" component={SkillsPage} />
             </Switch>
