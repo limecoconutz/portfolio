e909be725d37bb81d4e85c50381e380cca71d678
cosmina iosifescu <cosmina.iosifescu@gmail.com>
2021-09-15 16:36:00 +0200

    Changed to HashRouter so it works on deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import '../src/stylesheets/App.scss';
 import './stylesheets/App.scss';
 import Footer from  './components/Footer';
