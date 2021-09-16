735e58173e2b0e25b75b5dfadc3a994d36638689
cosmina iosifescu <cosmina.iosifescu@gmail.com>
2021-09-16 21:52:21 +0200

    changed sidebar styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,11 @@ function Sidebar() {
   return (
   // <section className="sidebar">
         <nav className="Sidebar">
-          <ul className="nav-links list"> 
+          <ul className="nav-links list">
+            <NavLink to='portfolio' exact className="nav-link" activeClassName="selected">
+               {/* <a className="" href="/portfolio"><i className="logo logo-bright fas fa-fan"></i></a> */}
+                <li className="list__elem text__bright"><i className="fas fa-fan"></i></li>
+              </NavLink>
             <NavLink to='MyProjects' exact className="nav-link" activeClassName="selected">
                 <li className="list__elem title text__bright">My Projects</li>
               </NavLink>
